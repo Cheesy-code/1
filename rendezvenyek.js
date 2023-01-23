39fa25ce0c04040dffcbb0a236c29ef3e6164028
Cheesy-code <norbi.sajtos@windowslive.com>
2023-01-23 16:43:28 +0100

    rendezvények animáció

---
+++ rendezvenyek.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#list").animate({
+        opacity: 1,
+        left: "+=50",
+        height: "toggle()"
+    }, 3000, function() {
+
+    })
+    //Felsorolás beúszás
+})
