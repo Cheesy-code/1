ed31f54124135c4248c31db281b7d94af38be8cd
Cheesy-code <norbi.sajtos@windowslive.com>
2023-01-24 16:15:39 +0100

    email proba 3

--- contact.js
+++ contact.js
@@ -1,34 +1,34 @@
-// $(document).ready(function() {
-//     $("#emailForm").on('submit', function(event) {
-//         event.preventDefault(); 
-//         var formData = $(this).serialize();
-//         $.ajax({
-//             type: 'POST',
-//             url: 'sendmail.php',
-//             dataType: "json",
-//             data: formData,
-//             success: function(response) { 
-//                 alert(response.success); 
-//             },
-//             error: function(xhr, status, error){
-//                 console.log(xhr); 
+$(document).ready(function () {
+    $("#emailForm").on('submit', function (event) {
+        event.preventDefault();
+        var formData = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'sendmail.php',
+            dataType: "json",
+            data: formData,
+            success: function (response) {
+                alert(response.success);
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr);
+            }
+        });
+    });
+});
+
+// $(document).ready(function () {
+//     //send button click
+//     $('#sendMail').on('click', function (e) {
+//         e.preventDefault();
+//         $.post('sendmail.php', $('#emailForm').serialize(), function (data) {
+//             //show the results if the mail was sent or not
+//             var res = $.parseJSON(data);
+//             if (res.result === true) {
+//                 alert('your mail was sent');
+//             } else {
+//                 alert('your mail was not sent');
 //             }
 //         });
 //     });
 // });
-
-$(document).ready(function () {
-    //send button click
-    $('#sendMail').on('click', function (e) {
-        e.preventDefault();
-        $.post('sendmail.php', $('#emailForm').serialize(), function (data) {
-            //show the results if the mail was sent or not
-            var res = $.parseJSON(data);
-            if (res.result === true) {
-               alert('your mail was sent');
-            }else {
-               alert('your mail was not sent');
-            }
-        });
-    });
-    });
