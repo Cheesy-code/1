0d3f3796186d75622eafd76c7bd55bdd7bbbf5d4
Cheesy-code <norbi.sajtos@windowslive.com>
2023-01-24 16:48:45 +0100

    mail

--- sendmail.php
+++ sendmail.php
@@ -1,21 +1,20 @@
 <?php
-// if (isset($_POST['message'])) {
-//     $to = 'norbi.sajtos@windowslive.com';
-// $subject = "..."; 
-//     $message = $_POST['message'];
-//     $headers = "From: " . $_POST['sender_nam‌​e​'] . " <" . $_POST['sender_em‌​ail‌​'] . ">\r\n";
-//     $headers = "Reply-To: " . $_POST['sender_ema‌​il‌​'] . "\r\n";
-//     $headers = "Content-type: text/html; charset=iso-8859-1\r\n";
-//     'X-Mailer: PHP/' . phpversion();
-//     if (mail($to, $subject, $message, $headers))
-//         echo json_encode(['success' => true]);
-//     else
-//         echo json_encode(['success' => false]);
-//     exit;
-// }
+if (isset($_POST['message'])) {
+    $to = 'norbi.sajtos@windowslive.com';
+$subject = "..."; 
+    $message = $_POST['message'];
+    $headers = "From: " . $_POST['sender_nam‌​e​'] . " <" . $_POST['sender_em‌​ail‌​'] . ">\r\n";
+    $headers = "Reply-To: " . $_POST['sender_ema‌​il‌​'] . "\r\n";
+    $headers = "Content-type: text/html; charset=iso-8859-1\r\n";
+    'X-Mailer: PHP/' . phpversion();
+    if (mail($to, $subject, $message, $headers))
+        echo json_encode(['success' => true]);
+    else
+        echo json_encode(['success' => false]);
+    exit;
+}
 
 // --------------------------------------------------------------------------------------
-//HTTPS
 
 // if ($_POST['send'] == 'SEND') {
 
@@ -34,39 +33,6 @@
 //     }
 // }
 
-
-// --------------------------------------------------------------------------------------
-
-
-// Pear Mail Library
-require_once "Mail.php";
-
-$from = '<norbi.sajtos@windowslive.com>';
-$to = '<norbi.sajtos@windowslive.com>';
-$subject = 'Hi!';
-$body = "Hi,\n\nHow are you?";
-
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
-
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'sajtos8265@gmail.com',
-        'password' => '573+2300-/'
-    ));
-
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-    echo('<p>' . $mail->getMessage() . '</p>');
-} else {
-    echo('<p>Message successfully sent!</p>');
-}
 
 // --------------------------------------------------------------------------------------
 
