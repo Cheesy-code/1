38a00795ee8a095d9fc4ffacc99b092adc525e01
Cheesy-code <norbi.sajtos@windowslive.com>
2023-01-24 15:56:39 +0100

    email proba 1

--- sendmail.php
+++ sendmail.php
@@ -16,28 +16,29 @@
 
 // --------------------------------------------------------------------------------------
 
-// if($_POST['send'] == 'SEND'){
+if ($_POST['send'] == 'SEND') {
 
-//     $to      = 'norbi.sajtos@windowslive.com';
-//     $subject = "asasd"; 
-//     $message = $_POST['message']; 
-//     $headers = "From: ".$_POST['sender_nam‌​e​']." <".$_POST['sender_em‌​ail‌​'].">\r\n"; $headers = "Reply-To: ".$_POST['sender_ema‌​il‌​']."\r\n"; 
-//     $headers = "Content-type: text/html; charset=iso-8859-1\r\n";
-//     'X-Mailer: PHP/' . phpversion();
-//     //check if the mail was sent
-//     if (mail($to, $subject, $message, $headers)) {
-//         echo json_encode(['result' => true]);
-//     }else {
-//         echo json_encode(['result' => false]);
-//      }
-//    }
+    $to = 'norbi.sajtos@windowslive.com';
+    $subject = "asasd";
+    $message = $_POST['message'];
+    $headers = "From: " . $_POST['sender_nam‌​e​'] . " <" . $_POST['sender_em‌​ail‌​'] . ">\r\n";
+    $headers = "Reply-To: " . $_POST['sender_ema‌​il‌​'] . "\r\n";
+    $headers = "Content-type: text/html; charset=iso-8859-1\r\n";
+    'X-Mailer: PHP/' . phpversion();
+    //check if the mail was sent
+    if (mail($to, $subject, $message, $headers)) {
+        echo json_encode(['result' => true]);
+    } else {
+        echo json_encode(['result' => false]);
+    }
+}
 
-   
+
 // --------------------------------------------------------------------------------------
 
-$msg = "Teszt üzenet.";
+// $msg = "Teszt üzenet.";
 
-mail("norbi.sajtos@windowslive.com", "My test object", $msg);
+// mail("norbi.sajtos@windowslive.com", "My test object", $msg);
 
 
 ?>
